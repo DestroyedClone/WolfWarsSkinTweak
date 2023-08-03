888ca9e4ed5f53c0eb7c95be1c52033e435b1f33
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2023-08-03 01:21:39 -0700

    Add project files.

---
+++ Util.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace AtOYoungSkins
+{
+    internal static class Util
+    {
+        public static Sprite CreateFlippedSprite(Sprite originalSprite)
+        {
+            if (originalSprite == null)
+            {
+                Debug.LogError("Original sprite is null!");
+                return null;
+            }
+
+            // Create a new Texture2D object with the same dimensions as the original sprite
+            Texture2D flippedTexture = new Texture2D((int)originalSprite.rect.width, (int)originalSprite.rect.height);
+
+            // Get the pixels from the original sprite
+            Color[] originalPixels = originalSprite.texture.GetPixels((int)originalSprite.textureRect.x,
+                                                                      (int)originalSprite.textureRect.y,
+                                                                      (int)originalSprite.textureRect.width,
+                                                                      (int)originalSprite.textureRect.height);
+
+            // Flip the pixels horizontally and copy them to the new Texture2D object
+            for (int y = 0; y < originalSprite.rect.height; y++)
+            {
+                for (int x = 0; x < originalSprite.rect.width; x++)
+                {
+                    flippedTexture.SetPixel((int)originalSprite.rect.width - x - 1, y, originalPixels[x + y * (int)originalSprite.rect.width]);
+                }
+            }
+
+            // Apply the changes to the new Texture2D
+            flippedTexture.Apply();
+
+            // Create a new sprite using the flipped Texture2D
+            Sprite flippedSprite = Sprite.Create(flippedTexture,
+                                                 new Rect(0, 0, flippedTexture.width, flippedTexture.height),
+                                                 new Vector2(0.5f, 0.5f),
+                                                 originalSprite.pixelsPerUnit,
+                                                 0,
+                                                 SpriteMeshType.FullRect);
+
+            return flippedSprite;
+        }
+    }
+}
---
+++ YSPlugin.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using AtOSkinExtender;
+using BepInEx;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine.Events;
+using System.Linq;
+using UnityEngine.Rendering.LookDev;
+using System.Runtime.CompilerServices;
+using BepInEx.Configuration;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace AtOYoungSkins
+{
+    [BepInPlugin("com.DestroyedClone.YoungSkins", "Young Skins", "0.1.0")]
+    [BepInDependency(AtOSkinExtender.Plugin.modGuid)]
+    [BepInDependency("com.DestroyedClone.FoeFacade", BepInDependency.DependencyFlags.SoftDependency)]
+
+    public class YSPlugin : BaseUnityPlugin
+    {
+        //youngskins cause youngbinks etc
+        //past skins? needs to be expandable if theres gonna be new settings
+        public const string identifier = "Young Skins";
+        public static List<SkinData> mySkins = new List<SkinData>();
+        public static BepInEx.Logging.ManualLogSource _logger;
+
+        public static ConfigEntry<bool> CfgRenameWolfWarSkins;
+
+        public static bool modloaded_foefacade = false;
+
+        public void Awake()
+        {
+            _logger = Logger;
+
+            CfgRenameWolfWarSkins = Config.Bind("", "Rename Wolfwar Skins", true, "If true, then the wolf war skins will be renamed from the generic The wolf wars to more appropriate names.");
+
+
+            AtOSkinExtender.Plugin.onCreateSkins += CreateEnemySkinsForHeroes;
+
+            modloaded_foefacade = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.DestroyedClone.FoeFacade");
+            On.MainMenuManager.Start += MainMenuManager_Start;
+        }
+
+        private void MainMenuManager_Start(On.MainMenuManager.orig_Start orig, MainMenuManager self)
+        {
+            orig(self);
+            RenameWolfWarSkins();
+        }
+
+        //youngbinks	Binks = ranger
+        //youngcharls	Charls = healer+lightingspell
+        //youngheiner	Model C
+        //youngmagnus	Magnus
+        //youngottis	Ottis
+        //youngyogger	Yogger
+
+        public SkinData DuplicateSkinForSubclass(SubClassData subClassData, SkinData skinToCopy, string sku = "2325780")
+        {
+            return Assets.CreateSkinData(subClassData.Id, $"{subClassData.SubClassName}"+skinToCopy.SkinName, 0,
+                skinToCopy.SpritePortrait, skinToCopy.SpritePortraitGrande,
+                skinToCopy.SpriteSilueta, skinToCopy.SpriteSiluetaGrande,
+                skinToCopy.SkinGo, sku); //wolfwars SKU
+        }
+
+        //[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        //spriteBorder = large portrait
+        //spriteBorderSmall = small portrait
+
+        public SkinData CreateSkinFromSubclassForSubclass(SubClassData guyToCopyFrom, SubClassData subToCopyTo, string sku = null)
+        {
+            return Assets.CreateSkinData(subToCopyTo.Id,
+                guyToCopyFrom.CharacterName,// + $" ({subToCopyTo.CharacterName})",
+                $"{subToCopyTo.SubClassName}" + "Skin" + guyToCopyFrom.SubClassName,
+                0,
+                guyToCopyFrom.SpriteSpeed, guyToCopyFrom.SpritePortrait,
+                guyToCopyFrom.SpriteBorderSmall, guyToCopyFrom.SpriteBorder,
+                guyToCopyFrom.GameObjectAnimated, sku ?? guyToCopyFrom.Sku); //wolfwars SKU
+        }
+
+        public SkinData CreateSkinFromNPCForSubclass(NPCData npc, SubClassData subclass, string sku = null)
+        {
+            return AtOSkinExtender.Assets.CreateSkinData(subclass.Id,
+                        npc.NPCName,
+                        $"{subclass.Id}Skin{npc.GameObjectAnimated.name}",
+                        0,
+                        npc.SpriteSpeed,
+                        npc.SpritePortrait,
+                        npc.SpriteSpeed,
+                        npc.SpritePortrait,
+                        npc.GameObjectAnimated,
+                        sku ?? "");
+        }
+
+        private void CreateEnemySkinsForHeroes()
+        {
+            CreateRusty();
+            //add to shooters
+            foreach (var subclass in Globals.Instance._SubClassSource)
+            {
+                //they dont HAVE skins
+                //var binksSkin = Globals.Instance.GetSkinsBySubclass("youngbinks")[0];
+                //var charlsSkin = Globals.Instance.GetSkinsBySubclass("youngcharls")[0];
+                _logger.LogMessage(subclass.Value.SubClassName);
+                switch (subclass.Value.HeroClass)
+                {
+                    case Enums.HeroClass.Warrior:
+                        break;
+                    case Enums.HeroClass.Mage:
+                        break;
+                    case Enums.HeroClass.Healer:
+                        mySkins.Add(CreateSkinFromSubclassForSubclass(Globals.Instance.GetSubClassData("youngcharls"), subclass.Value, AtOSkinExtender.Assets.sku_WolfWars));
+                        break;
+                    case Enums.HeroClass.Scout:
+                        mySkins.Add(CreateSkinFromSubclassForSubclass(Globals.Instance.GetSubClassData("youngbinks"), subclass.Value));
+                        break;
+                    case Enums.HeroClass.MagicKnight: //unused
+                        break;
+                    case Enums.HeroClass.Monster: //seemingly used for corruptions/petmonsters(?)
+                        break;
+                }
+            }
+            RegisterSkins();
+        }
+
+        private void RenameWolfWarSkins()
+        {
+            if (!CfgRenameWolfWarSkins.Value) return;
+
+            //Globals.Instance.GetSkinData("magnuswolfwars").SkinName = Texts.Instance.GetText("wolfWars");
+            Globals.Instance.GetSkinData("heinerwolfwars").SkinName = Globals.Instance.GetSubClassData("youngheiner").CharacterName;
+            Globals.Instance.GetSkinData("breewolfwars").SkinName = Globals.Instance.GetNPC("balina").NPCName;
+            //Globals.Instance.GetSkinData("ottiswolfwars").SkinName = Texts.Instance.GetText("wolfWars");
+        }
+
+        private void CreateRusty()
+        {
+            if (modloaded_foefacade)
+                return;
+            mySkins.Add(CreateSkinFromNPCForSubclass(Globals.Instance.GetNPC("rusty"), Globals.Instance.GetSubClassData("sentinel")));
+        }
+
+        private void RegisterSkins()
+        {
+            AtOSkinExtender.Assets.AddSkinDataToPack(mySkins, identifier);
+        }
+    }
+}
